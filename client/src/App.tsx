1599706bc299142c95cf1a1b99d938ae38769dc6
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-23 15:10:27 +0530

    revamped

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
 import { Button } from './components/ui/button';
-import toast from 'react-hot-toast';
+import useShowToast from './utils/Toast';
 
 const App = () => {
+  const { showSuccess, showError, showPromise } = useShowToast();
+
+  const saveSettings = (isSuccess: boolean) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (isSuccess) {
+          resolve('Settings saved successfully');
+        } else {
+          reject('Could not save settings');
+        }
+      }, 2000);
+    });
+  };
+
+  const handlePromiseClick = () => {
+    const promise = saveSettings(true);
+
+    showPromise(promise, {
+      loading: 'Saving...',
+      success: 'Settings saved!',
+      error: 'Could not save.',
+    });
+  };
+
   return (
     <>
-      <div className='flex w-full h-screen items-center justify-center'>
-        <Button size={'lg'} onClick={() => toast.success('worked')}>
-          Hello
+      <div className='flex w-full h-screen items-center justify-center gap-5 bg-slate-100'>
+        <Button
+          size={'lg'}
+          onClick={() => showSuccess('worked')}
+          className='bg-teal-600 hover:bg-teal-700'
+        >
+          Success
+        </Button>
+        <Button
+          size={'lg'}
+          variant={'destructive'}
+          onClick={() => showError('Error')}
+        >
+          Error
+        </Button>
+        <Button size={'lg'} onClick={handlePromiseClick}>
+          Promise
         </Button>
       </div>
     </>
--- client/src/utils/Toast.ts
+++ client/src/utils/Toast.ts
@@ -1,3 +1,36 @@
 import toast from 'react-hot-toast';
 
-const shoeToast;
+const useShowToast = () => {
+  const showSuccess = (message: string) => {
+    toast.success(message);
+  };
+
+  const showError = (message: string) => {
+    toast.error(message);
+  };
+
+  const showPromise = <T>(
+    promise: Promise<T>,
+    options: {
+      loading?: string;
+      success?: string;
+      error?: string;
+    } = {}
+  ) => {
+    const {
+      loading = 'Loading...',
+      success = 'Success!',
+      error = 'Error!',
+    } = options;
+
+    return toast.promise(promise, {
+      loading,
+      success,
+      error,
+    });
+  };
+
+  return { showSuccess, showError, showPromise };
+};
+
+export default useShowToast;
