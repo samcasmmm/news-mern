1d78da75e4c168c3050d3af9c04115cf091c411d
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-25 16:16:33 +0530

    bug/authMiddleware not getting user --solved

--- api/middlewares/AuthMiddleware.js
+++ api/middlewares/AuthMiddleware.js
@@ -12,6 +12,7 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.SECRET_KEY);
       req.user = await User.findById(decoded.userId).select('-password');
+      console.log(req.user);
       next();
     } catch (error) {
       console.error(error);
@@ -25,12 +26,32 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-const admin = (req, res, next) => {
-  if (req.user && req.user.userType === 'admin') {
-    next();
-  } else {
-    res.status(401);
-    throw new Error('Not authorized as an Admin');
+const admin = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      req.user = await User.findById(decoded.userId).select('-password');
+      console.log(req.user);
+      if (req.user.userType === 'admin') {
+        next();
+      } else {
+        res.status(401);
+        throw new Error('Not Authorized, No Access');
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Not Authorized, Token Failed');
+    }
   }
-};
+  if (!token) {
+    res.status(401);
+    throw new Error('Not Authorized, No Token');
+  }
+});
 export { protect, admin };
