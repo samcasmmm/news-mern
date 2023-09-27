0ee1a6cf789e53cc2009f11114d1992a97cda02f
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-27 21:56:56 +0530

    fallback

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -10,21 +10,25 @@ import {
 
 const Navbar = () => {
   return (
-    <nav className='container flex flex-row items-center justify-between py-4 bg-dark-box'>
-      <p className=''>Logo</p>
-      <div className='flex items-center justify-center'>
-        <ThemeSwitcher />
-        <DropdownMenu>
-          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-          <DropdownMenuContent className='bg-dark'>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <nav className='bg-white dark:bg-dark-box'>
+      <div className='container flex flex-row items-center justify-between py-4 '>
+        <p className='dark:text-white text-black'>Logo</p>
+        <div className='flex items-center justify-center gap-4'>
+          <DropdownMenu>
+            <DropdownMenuTrigger className='hover:text-primarytext'>
+              Categories
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='dark:bg-dark'>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <ThemeSwitcher />
+        </div>
       </div>
     </nav>
   );
