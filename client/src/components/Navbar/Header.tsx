cd6cef420cc4aee9222ca334e4251814771c5139
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-28 22:15:43 +0530

    update header and nav

--- client/src/components/Navbar/Header.tsx
+++ client/src/components/Navbar/Header.tsx
@@ -39,11 +39,11 @@ const Header = () => {
   }
   const { formattedDate } = getCurrentDate();
   return (
-    <div className='bg-slate-200 dark:bg-dark-box2 p-4'>
+    <div className='bg-gray-200 dark:bg-dark-box2 p-4'>
       <div className='container flex flex-row justify-between px-0 xsm:px-4'>
         <div className='flex flex-row items-center justify-center gap-4'>
           <Clock4 color='white' className='fill-blue-600' />
-          <p className='text-sm text-black'>{formattedDate}</p>
+          <p className='text-sm text-black dark:text-white'>{formattedDate}</p>
         </div>
         <div className='flex flex-row items-center justify-center gap-4'>
           <Facebook className='cursor-pointer hover:fill-blue-500 scale-75 hover:scale-110 ease-in-out  transition duration-300  text-gray-600 dark:text-white' />
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import DropDownComponent from '../common/DropDownComponent';
+import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 
 const menuItems = [
@@ -18,20 +19,21 @@ const menuItems = [
 
 const Navbar = () => {
   return (
-    <nav className='bg-white dark:bg-dark-box shadow dark:shadow-slate-950'>
-      <div className='container flex flex-row items-center justify-between py-4 '>
-        <p className='dark:text-white text-black'>Logo</p>
-        <div className='flex items-center justify-center gap-4'>
+    <nav className='bg-white dark:bg-dark-box shadow dark:shadow-slate-950 flex sm:flex-row flex-col items-center justify-center'>
+      <div className='container flex flex-row items-center justify-between py-4 flex-1 order-3'>
+        <div className='flex items-center justify-between sm:justify-center gap-4 w-full'>
+          <p className='dark:text-white text-black hidden sm:block'>Logo</p>
           <DropDownComponent title={'Trending'} list={menuItems} />
           <DropDownComponent title={'Categories'} list={menuItems} />
         </div>
-        <div className='flex flex-row items-center justify-center'>
-          <Input
-            type='text'
-            className='focus:ring-offset-0 focus-visible:ring-offset-0 bg-white dark:bg-dark border-slate-700'
-          />
-          icon
-        </div>
+      </div>
+      <p className='dark:text-white text-black block sm:hidden order-1'>Logo</p>
+      <div className='flex flex-row items-center justify-center p-4 gap-2 flex-1 order-3 w-full'>
+        <Input
+          type='text'
+          className='focus:ring-offset-0 focus-visible:ring-offset-0 bg-white dark:bg-dark border-slate-700'
+        />
+        <Button>Search</Button>
       </div>
     </nav>
   );
