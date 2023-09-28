b832647711d0e4fbbe2b00973d55dafee3a63d82
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-28 22:23:15 +0530

    update header and nav

--- client/src/components/ThemeSwitcher.tsx
+++ client/src/components/ThemeSwitcher.tsx
@@ -15,7 +15,7 @@ const ThemeSwitcher = () => {
           }
         }}
       />
-      <div className="h-6 w-11 rounded-full bg-gray-200 bg-gray-500 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white dark:border-gray-600 dark:bg-gray-700"></div>
+      <div className="h-6 w-11 rounded-full bg-gray-500 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white dark:border-gray-600 dark:bg-gray-700"></div>
     </label>
   );
 };
