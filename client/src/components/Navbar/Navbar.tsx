0634cb23449f1aebe39476cf05d984cb76e59ba7
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-29 22:00:41 +0530

    responsive navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import DropDownComponent from '../common/DropDownComponent';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
+import { useState, useEffect } from 'react';
 
 const menuItems = [
   {
@@ -18,24 +19,41 @@ const menuItems = [
 ];
 
 const Navbar = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const handleToggle = () => setToggleNav(!toggleNav);
+  const closeToggle = () => setToggleNav(false);
+  // console.log(window.scroll > 10);
+  useEffect(() => {
+    window.addEventListener('scroll', closeToggle);
+    return () => {
+      window.removeEventListener('scroll', closeToggle);
+    };
+  });
+
   return (
-    <nav className="flex flex-col items-center justify-center bg-white shadow dark:bg-dark-box dark:shadow-slate-950 sm:flex-row">
-      <div className="container order-3 flex flex-1 flex-row items-center justify-between py-4">
-        <div className="flex w-full items-center justify-between gap-4 sm:justify-center">
-          <p className="hidden cursor-pointer text-black dark:text-white sm:block">
-            Logo
-          </p>
-          <DropDownComponent title={'Trending'} list={menuItems} />
-          <DropDownComponent title={'Categories'} list={menuItems} />
+    <nav className="relative flex w-full flex-row items-center justify-center bg-white shadow dark:bg-dark-box dark:shadow-slate-950">
+      <div className="container flex w-full justify-between p-4">
+        <p>Logo</p>
+        <div className="block" onClick={handleToggle}>
+          open
         </div>
       </div>
-      <p className="flex-1 text-black  dark:text-white">Logo</p>
-      <div className="flex w-full flex-1 flex-row items-center justify-center gap-2 p-4">
-        <Input
-          type="text"
-          className="border-slate-700 bg-white focus:ring-offset-0 focus-visible:ring-offset-0 dark:bg-dark"
-        />
-        <Button>Search</Button>
+      <div
+        className={`fixed ${
+          toggleNav ? 'right-0' : 'right-[-120%]'
+        } top-0 flex h-[100vh] w-[100vw] flex-col items-end bg-gray-900/20 transition-all duration-500 ease-in-out`}
+      >
+        <div className="h-full w-full bg-slate-800 md:w-3/4">
+          <button onClick={handleToggle}>close</button>
+          <div className="order-3  flex w-full flex-1 flex-row items-center justify-center gap-2 p-4 md:order-none">
+            <Input
+              type="text"
+              className="border-slate-700 bg-white focus:ring-offset-0 focus-visible:ring-offset-0 dark:bg-dark "
+            />
+            <Button>Search</Button>
+          </div>
+        </div>
       </div>
     </nav>
   );
