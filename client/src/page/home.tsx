dc9bf58fe32f900244397f7450749d9ce5195b92
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-29 22:26:06 +0530

    responsive navbar

--- client/src/page/home.tsx
+++ client/src/page/home.tsx
@@ -1,10 +1,10 @@
 const Home = () => {
   return (
     <>
-      <div className='h-screen-ios relative z-20 mx-auto flex h-screen max-w-7xl flex-col justify-between px-4 overflow-x-hidden'></div>
-      {/* <img
-        src='https://react.email/_next/image?url=%2Fstatic%2Fbg.png&w=1080&q=75'
-        className='absolute top-[220px] left-0 z-[10] h-full w-full select-none md:top-0'
+      <div className="h-screen-ios relative z-20 mx-auto flex h-screen max-w-7xl flex-col justify-between overflow-x-hidden px-4"></div>
+      <img
+        src="https://react.email/_next/image?url=%2Fstatic%2Fbg.png&w=1080&q=75"
+        className="absolute left-0 top-[220px] z-[-1] h-full w-full animate-pulse select-none md:top-0"
         style={{
           position: 'absolute',
           height: '100%',
@@ -14,8 +14,9 @@ const Home = () => {
           right: 0,
           bottom: 0,
           color: 'transparent',
+          filter: 'blur:100%',
         }}
-      /> */}
+      />
     </>
   );
 };
