4f06cf59d075ab3aa257f87a452342dcbf01f80b
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-29 22:27:46 +0530

    responsive navbar

--- client/src/page/home.tsx
+++ client/src/page/home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
       <div className="h-screen-ios relative z-20 mx-auto flex h-screen max-w-7xl flex-col justify-between overflow-x-hidden px-4"></div>
       <img
         src="https://react.email/_next/image?url=%2Fstatic%2Fbg.png&w=1080&q=75"
-        className="absolute left-0 top-[220px] z-[-1] h-full w-full animate-pulse select-none md:top-0"
+        className="absolute left-0 top-[220px] z-[-1] h-full w-full  select-none blur-3xl md:top-0"
         style={{
           position: 'absolute',
           height: '100%',
@@ -14,7 +14,6 @@ const Home = () => {
           right: 0,
           bottom: 0,
           color: 'transparent',
-          filter: 'blur:100%',
         }}
       />
     </>
