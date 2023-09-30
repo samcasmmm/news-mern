e3ffb0a2c2fcd5d0e03bd1975d2d1b9d28c42d98
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-30 17:17:35 +0530

    side bar

---
+++ client/src/components/RecentNews.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const RecentNews = () => {
+  return (
+    <div className="flex h-full w-full flex-col">
+      <div className="w-full rounded-lg bg-white p-4 shadow shadow-lg">
+        <img
+          src="https://images.unsplash.com/photo-1682685797303-0ad51eb23e13?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          alt=""
+          className="w-full brightness-50"
+        />
+        <p>hello</p>
+      </div>
+    </div>
+  );
+};
+
+export default RecentNews;
--- client/src/page/home.tsx
+++ client/src/page/home.tsx
@@ -1,7 +1,15 @@
+import RecentNews from '../components/RecentNews';
+
 const Home = () => {
   return (
     <>
-      <div className="h-screen-ios relative z-10 mx-auto flex h-screen max-w-7xl flex-col justify-between overflow-x-hidden px-4"></div>
+      <div className="container mt-4 flex h-20 w-full flex-row px-4">
+        <div className="hidden h-20 w-full flex-1 bg-red-500 md:block"></div>
+        <div className="h-20 w-full flex-[3] bg-teal-500">
+          <RecentNews />
+        </div>
+        <div className="hidden h-20 w-full flex-1  bg-red-500 md:block"></div>
+      </div>
     </>
   );
 };
