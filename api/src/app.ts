ed8d799542603639618e0af3cf791e01d9164664
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-10-14 00:15:40 +0530

    app.ts

---
+++ api/src/app.ts
@@ -0,0 +1,46 @@
+import express,{Application} from 'express'
+import mongoose from 'mongoose'
+import compression from 'compression';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+import Controller from '@/utils/interfaces/controller.interface.ts';
+import ErrorMiddleware from '@/middlewares/error.middleware.ts';
+
+class App {
+   public express:Application;
+   public port: number;
+
+   constructor(controllers:Controller[], port: number) {
+      this.express = express();
+      this.port = port;
+      
+      this.initialiseDatabaseConnection();
+      this.initialiseMiddleware();
+      this.initialiseControllers(controllers);
+      this.initialiseErrorHandling();
+      
+   }
+
+   private initialiseDatabaseConnection(): void {
+      const {MONGO_PATH, MONGO_USER, MONGO_PASSWORD}  = process.env
+      mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${}`)
+   }
+   private initialiseMiddleware(): void {
+      this.express.use(helmet())
+      this.express.use(cors())
+      this.express.use(morgan('dev'))
+      this.express.use(express.json())
+      this.express.use(express.urlencoded({extended:false}))
+      this.express.use(compression())
+   }
+   private initialiseControllers(controllers:Controller[]): void {
+      controllers.forEach((controller:Controller) => {
+         this.express.use('/api',controller.router)
+      })
+   }
+   private initialiseErrorHandling(): void {
+         this.express.use(ErrorMiddleware())
+   }
+}
