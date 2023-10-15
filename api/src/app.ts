a333bd162827d2d76ac0af97791325bae8a156a4
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-10-15 16:05:22 +0530

    added middlewares

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,5 @@
-import express,{Application} from 'express'
-import mongoose from 'mongoose'
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
 import compression from 'compression';
 import cors from 'cors';
 import morgan from 'morgan';
@@ -9,46 +9,46 @@ import Controller from '@/utils/interfaces/controller.interface';
 import ErrorMiddleware from '@/middlewares/error.middleware';
 
 class App {
-   public express:Application;
-   public port: number;
+  public express: Application;
+  public port: number;
 
-   constructor(controllers:Controller[], port: number) {
-      this.express = express();
-      this.port = port;
-      
-      this.initialiseDatabaseConnection();
-      this.initialiseMiddleware();
-      this.initialiseControllers(controllers);
-      this.initialiseErrorHandling();
-      
-   }
+  constructor(controllers: Controller[], port: number) {
+    this.express = express();
+    this.port = port;
 
-   private initialiseDatabaseConnection(): void {
-      const {MONGO_PATH, MONGO_USER, MONGO_PASSWORD}  = process.env
-      mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`)
-   }
-   private initialiseMiddleware(): void {
-      this.express.use(helmet())
-      this.express.use(cors())
-      this.express.use(morgan('dev'))
-      this.express.use(express.json())
-      this.express.use(express.urlencoded({extended:false}))
-      this.express.use(compression())
-   }
-   private initialiseControllers(controllers:Controller[]): void {
-      controllers.forEach((controller:Controller) => {
-         this.express.use('/api',controller.router)
-      })
-   }
-   private initialiseErrorHandling(): void {
-         this.express.use(ErrorMiddleware())
-   }
-   public listen():void {
-      this.express.listen(this.port,()=>{
-         console.log(`${process.env.NODE_ENV} Server http://localhost:9000`)
-      })
-   }
+    this.initialiseDatabaseConnection();
+    this.initialiseMiddleware();
+    this.initialiseControllers(controllers);
+    this.initialiseErrorHandling();
+  }
+
+  private initialiseDatabaseConnection(): void {
+    const { MONGO_PATH, MONGO_USER, MONGO_PASSWORD } = process.env;
+    mongoose.connect(
+      `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`
+    );
+  }
+  private initialiseMiddleware(): void {
+    this.express.use(helmet());
+    this.express.use(cors());
+    this.express.use(morgan('dev'));
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: false }));
+    this.express.use(compression());
+  }
+  private initialiseControllers(controllers: Controller[]): void {
+    controllers.forEach((controller: Controller) => {
+      this.express.use('/api', controller.router);
+    });
+  }
+  private initialiseErrorHandling(): void {
+    this.express.use(ErrorMiddleware);
+  }
+  public listen(): void {
+    this.express.listen(this.port, () => {
+      console.log(`${process.env.NODE_ENV} Server http://localhost:9000`);
+    });
+  }
 }
 
-
-export default App
+export default App;
