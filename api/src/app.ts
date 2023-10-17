0a90b916d4025aabfb3fb0fadc58aa88ee58e561
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-10-17 20:27:28 +0530

    connection to db

---
+++ api/src/app.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
---
+++ api/src/utils/config/connectDB.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDatabase = async () => {
+  const {} = process.env;
+  mongoose.connect(``);
+  //   try {
+  //     const conn = await mongoose.connect(process.env.MONGODB_URL, {
+  //       useUnifiedTopology: true,
+  //       useNewUrlParser: true,
+  //     });
+  //     console.log(`MongoDB Connected`);
+  //   } catch (error) {
+  //     console.error(`Error: ${error.message}`);
+  //     process.exit(1);
+  //   }
+};
+
+export default connectDatabase;
