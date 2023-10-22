beee398b2d057798ad474c04aa1b762d5f7e0c6d
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-10-22 15:58:16 +0530

    first router

--- api/src/utils/exceptions/http.exception.ts
+++ api/src/utils/exceptions/http.exception.ts
@@ -12,3 +12,19 @@ class HttpException extends Error {
 }
 
 export default HttpException;
+
+//
+type HttpException1 = (message: string) => {
+  statusCode: number;
+  message: string;
+};
+
+const createHttpException: (statusCode: number) => HttpException1 =
+  (statusCode) => (message) => ({
+    statusCode,
+    message,
+  });
+
+export const NotFoundException: HttpException1 = createHttpException(404);
+export const BadRequestException: HttpException1 = createHttpException(400);
+export const UnauthorizedException: HttpException1 = createHttpException(401);
