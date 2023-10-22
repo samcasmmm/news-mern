ecb27ebfd29e288a3f35202bf70abd2ab341ddaa
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-10-22 15:55:26 +0530

    first router

--- api/src/resources/users/users.model.ts
+++ api/src/resources/users/users.model.ts
@@ -1,1 +1,12 @@
-import { Schema } from 'mongoose';
+import mongoose, { Model, Schema } from 'mongoose';
+import Users from './users.interface.js';
+
+const userSchema = new Schema<Users>({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const UserModel: Model<Users> = mongoose.model<Users>('User', userSchema);
+
+export default UserModel;
