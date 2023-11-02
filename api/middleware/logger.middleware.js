073f08d21d9f37d2b9bce4f77f5c374c9b72ba35
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-11-02 20:06:25 +0530

    middleware fix

--- api/middleware/logger.middleware.js
+++ api/middleware/logger.middleware.js
@@ -3,24 +3,18 @@ import path from 'path';
 const HttpLogger = (req, res, next) => {
   const logData = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
 
-  // Define the log folder
   const logFolder = './logger';
 
-  // Create the "logger" folder if it doesn't exist
   if (!fs.existsSync(logFolder)) {
     fs.mkdirSync(logFolder);
   }
 
-  // Get the current date in YYYYMMDD format
   const currentDate = new Date().toISOString().slice(0, 10).replace(/-/g, '');
 
-  // Define the log file name using the current date
   const logFileName = `request-${currentDate}.log`;
 
-  // Construct the full log file path
   const logFilePath = path.join(logFolder, logFileName);
 
-  // Append the log data to the log file
   fs.appendFile(logFilePath, logData, (err) => {
     if (err) {
       console.error('Error logging request:', err);
