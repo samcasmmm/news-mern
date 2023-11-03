672b67507f415ec3246a78b0bade07bbbcffc668
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-11-03 22:25:49 +0530

    posts.model.js

---
+++ api/models/posts.model.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  headerImage: {
+    type: String, // You can store the image URL as a string
+    required: true,
+  },
+  content: [
+    {
+      type: {
+        // Type can be 'image' or 'paragraph'
+        type: String,
+        enum: ['image', 'paragraph'],
+        required: true,
+      },
+      value: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      text: {
+        type: String,
+        required: true,
+      },
+      author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
