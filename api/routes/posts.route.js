fd0390096b4339c095aea2c796a4b30a3ce00902
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-02 19:36:27 +0530

    fix posts.route.js

--- api/routes/posts.route.js
+++ api/routes/posts.route.js
@@ -12,10 +12,10 @@ import { protect } from '../middleware/auth.middleware.js';
 const router = express.Router();
 // POST route for creating a new post, using the controller function
 
-router.post('/posts', createPost);
+router.post('/', createPost);
 router.put('/posts/:id', updatePost);
 router.delete('/posts/:id', deletePost);
-router.get('/posts', getAllPosts);
+router.get('/posts/all', getAllPosts);
 router.get('/posts/:id', getPostById);
 router.get('/posts/search', searchPosts);
 
