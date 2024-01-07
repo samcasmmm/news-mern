3b54bffe55fae24c13f81d81bdb8b10d28885e75
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-07 21:02:14 +0530

    added category.model.js

--- api/models/category.model.js
+++ api/models/category.model.js
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose';
 
+const counterSchema = new mongoose.Schema({
+   _id: { type: String, required: true },
+   seq: { type: Number, default: 0 }
+});
+
+const Counter = mongoose.model('Counter', counterSchema);
+
 const categorySchema = new mongoose.Schema({
-   id: {
+   categoryId: {
       type: Number,
+      unique: true
    },
    name: {
       type: String,
@@ -18,5 +26,22 @@ const categorySchema = new mongoose.Schema({
       default: Date.now
    }
 });
+
+categorySchema.pre('save', function (next) {
+   const doc = this;
+   Counter.findByIdAndUpdate(
+      { _id: 'categoryId' },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+   )
+      .then((counter) => {
+         doc.categoryId = counter.seq;
+         next();
+      })
+      .catch((error) => {
+         throw new Error(error);
+      });
+});
+
 const Category = mongoose.model('Category', categorySchema);
-export default Category 
+export default Category;
