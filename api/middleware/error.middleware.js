ce969fa510bcae8c839c79add03c6e3548140098
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-01-13 19:18:11 +0530

    fixing error.middleware.js

--- api/middleware/error.middleware.js
+++ api/middleware/error.middleware.js
@@ -10,6 +10,7 @@ const errorHandler = (err, req, res, next) => {
   res.status(statusCode).json({
     status: statusCode,
     message: err.message,
+    url: req.url,
     meta: res.meta || null,
     data: null,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
