d0c10df7429a39b74e2cde5ec556b721fde34d15
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-02-20 19:45:56 +0530

    server in ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,23 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 
+import "module-alias/register";
+
+import { Print } from "@/utils/index";
+
 const app: Application = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(compression());
 
 const listenServer = async () => {
   app.listen(7000, () => {
     console.log(`Server at localhost:7000`);
   });
 };
+
+listenServer();
