868e11b4a177340b0b9988000c25ef6ce42aa0b4
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-02-20 19:51:37 +0530

    server in ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,31 @@
-import express, { Application } from "express";
-import mongoose from "mongoose";
-import compression from "compression";
-import cors from "cors";
-import morgan from "morgan";
-import helmet from "helmet";
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import compression from 'compression';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
-import "module-alias/register";
+import 'module-alias/register';
 
-import { Print } from "@/utils/index";
+import { Print } from '@/utils/index';
 
 const app: Application = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 
-app.get("/", (req, res) => {
-  res.json({ msg: Print() });
+app.get('/', (req, res) => {
+    res.json({ msg: Print() });
 });
 
 const listenServer = async () => {
-  app.listen(7000, () => {
-    console.log(`Server at http://localhost:7000`);
-  });
+    app.listen(7000, () => {
+        console.log(`Server at http://localhost:7000`);
+    });
 };
 
 listenServer();
