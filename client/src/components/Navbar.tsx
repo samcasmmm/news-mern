d986042467e177789c12e5ae175cd5954d235cd3
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-03-13 22:04:47 +0530

    client

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Bell, Search } from '@/components/common/Icons';
-import Avatar from './common/Avatar';
+import AvatarWrapper from './common/Avatar';
+import IconWrapper from './common/IconWrapper';
 const Navbar = () => {
   return (
     <div className="border-b border-b-gray-300 bg-white p-2">
@@ -7,10 +8,13 @@ const Navbar = () => {
         <div className="">
           <img src="/logo_dark.png" alt="logo" className="w-32" />
         </div>
-        <div className="flex items-center justify-center space-x-3">
-          <Search />
-          <Bell />
-          <Avatar />
+        <div className="flex items-center justify-center space-x-2">
+          <IconWrapper icon={Search} />
+          <IconWrapper icon={Bell} />
+          <AvatarWrapper
+            src="https://miro.medium.com/v2/resize:fill:64:64/0*LZxbvU7VIIZ3RMFi"
+            alt=""
+          />
         </div>
       </div>
     </div>
--- client/src/components/common/Avatar.tsx
+++ client/src/components/common/Avatar.tsx
@@ -4,13 +4,19 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 type Props = {
   src: string;
   alt: string;
-  className: string;
-  onClick: () => void;
+  className?: string;
+  onClick?: () => void;
 };
 
 const AvatarWrapper = ({ src, alt, className, onClick }: Props) => {
   return (
-    <Avatar className={cn('', className)} onClick={onClick}>
+    <Avatar
+      className={cn(
+        'cursor-pointer border-2 transition-all duration-300 hover:border-blue-400',
+        className,
+      )}
+      onClick={onClick}
+    >
       <AvatarImage src={src} alt={alt} />
       <AvatarFallback>{alt}</AvatarFallback>
     </Avatar>
