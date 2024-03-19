946cfec2b764f4260b2c71a3e3571d7608b8c5db
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-03-19 23:22:43 +0530

    remove carousel

--- client/src/page/home.tsx
+++ client/src/page/home.tsx
@@ -2,13 +2,21 @@ import { toggleModal } from '@/app/features/UIState.slice';
 import Modal from '@/components/common/Modal';
 import { Button } from '@/components/ui/button';
 import { useAppDispatch } from '@/hooks/useAppState';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   return (
     <div className="flex h-screen w-full items-center justify-center">
       Main Content
       <Button onClick={() => dispatch(toggleModal())}>Open</Button>
+      <Button>
+        <Link to="/about">About</Link>
+      </Button>
+      <Button>
+        <Link to="/contact">Contact</Link>
+      </Button>
       <Modal
         title="testing"
         description=" Make changes to our profile here. Click save when you're done."
--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,12 +1,24 @@
 import { lazy } from 'react';
 
 const Home = lazy(() => import('../page/Home.tsx'));
+const About = lazy(() => import('../page/About.tsx'));
+const Contact = lazy(() => import('../page/Contact.tsx'));
 
 const coreRoutes = [
   {
     path: '/',
     title: 'Home',
     component: Home,
+  },
+  {
+    path: '/about',
+    title: 'About',
+    component: About,
+  },
+  {
+    path: '/contact',
+    title: 'Contact',
+    component: Contact,
   },
 ];
 
