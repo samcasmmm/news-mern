b724479f47864aa70fbaeb28a980de83e63ff589
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-03-21 22:59:54 +0530

    signUp

--- client/src/page/SignUp.tsx
+++ client/src/page/SignUp.tsx
@@ -33,12 +33,12 @@ const SignUp = () => {
 
   const FormHeading = () => (
     <div className="mb-10 flex flex-col items-center justify-center gap-2">
-            <p className="text-2xl font-bold text-emerald-500">
-              Signin to Your Account
-            </p>
-            <div className="h-1 w-12 rounded-full bg-emerald-500" />
-          </div>
-  )
+      <p className="text-2xl font-bold text-emerald-500">
+        Signin to Your Account
+      </p>
+      <div className="h-1 w-12 rounded-full bg-emerald-500" />
+    </div>
+  );
 
   const Footer = () => (
     <div className="flex items-center justify-center gap-2 bg-white p-2">
@@ -57,12 +57,12 @@ const SignUp = () => {
     <div className="flex h-screen flex-col items-center justify-center">
       <div className="flex h-screen w-full flex-col items-center justify-center bg-white">
         <div className="flex h-screen w-5/6 flex-col items-center justify-center gap-4 bg-white md:w-3/6 lg:w-2/12">
-          
+        <FormHeading/>
           <Input
             type="text"
             placeholder="name"
             name="name"
-            className="rounded-md p-2 ring-offset-emerald-400 focus-visible:ring-1 focus-visible:ring-emerald-400 focus-visible:ring-offset-1 font-semibold"
+            className="rounded-md p-2 font-semibold ring-offset-emerald-400 focus-visible:ring-1 focus-visible:ring-emerald-400 focus-visible:ring-offset-1"
             value={inputData.name}
             onChange={handleInputChange}
           />
@@ -70,7 +70,7 @@ const SignUp = () => {
             type="text"
             placeholder="email"
             name="email"
-            className="rounded-md p-2 ring-offset-emerald-400 focus-visible:ring-1 focus-visible:ring-emerald-400 focus-visible:ring-offset-1 font-semibold"
+            className="rounded-md p-2 font-semibold ring-offset-emerald-400 focus-visible:ring-1 focus-visible:ring-emerald-400 focus-visible:ring-offset-1"
             value={inputData.email}
             onChange={handleInputChange}
           />
@@ -78,20 +78,20 @@ const SignUp = () => {
             type="password"
             placeholder="password"
             name="password"
-            className="rounded-md p-2 ring-offset-emerald-400 focus-visible:ring-1 focus-visible:ring-emerald-400 focus-visible:ring-offset-1 font-semibold"
+            className="rounded-md p-2 font-semibold ring-offset-emerald-400 focus-visible:ring-1 focus-visible:ring-emerald-400 focus-visible:ring-offset-1"
             value={inputData.password}
             onChange={handleInputChange}
           />
           <Button
             onClick={handleSignInClick}
-            className="w-full rounded-md bg-greenEm hover:bg-emerald-600 font-semibold"
+            className="w-full rounded-md bg-greenEm font-semibold hover:bg-emerald-600"
           >
             SignUp
           </Button>
           <p className="">
             Don't have account ?{' '}
-            <strong className="cursor-pointer text-emerald-500 duration-200 ease-linear hover:text-emerald-700" >
-             <Link to={'/signIn'}>SignIn</Link>
+            <strong className="cursor-pointer text-emerald-500 duration-200 ease-linear hover:text-emerald-700">
+              <Link to={'/signIn'}>SignIn</Link>
             </strong>
           </p>
         </div>
